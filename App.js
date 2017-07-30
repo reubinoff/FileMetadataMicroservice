fa7eef1fb7ef7d92472b3a2119f81d6c5807a023
Moshe Reubinoff <Moshe.Reubinoff@harmonicinc.com>
2017-07-31 00:28:49 +0300

    finish integration with multer

--- App.js
+++ App.js
@@ -4,18 +4,25 @@ var logger = require('winston')
 var bodyParser = require('body-parser');
 
 var morgan = require('morgan')
+
+var multer = require('multer')
+var upload = multer({ dest: '/tmp/myapp/' })
 const server_config = require('./config/server_config')
 
 
-logger.add(logger.transports.File, { filename: "/tmp/"+server_config.app_name+".log" })
+logger.add(logger.transports.File, { filename: "/tmp/" + server_config.app_name + ".log" })
 
 logger.info("Application started!")
 
 
 // set static pages
-const pages_path = __dirname + "/www" 
+const pages_path = __dirname + "/www"
 app.set('view engine', 'pug')
 app.set('views', pages_path)
+app.set('view options', {
+  layout: false
+});
+
 logger.log(express.static(pages_path))
 
 
@@ -24,9 +31,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+//router
+
+app.post('/file', upload.single('avatar'), function (req, res, next) {
+  
+  // req.file is the `avatar` file 
+  // req.body will hold the text fields, if there were any 
+  const size = req.file.size
+  res.json({size});
+})
+
 // index page
-app.get('/', function(req, res){
-  res.render('index', {
+app.get('/', function (req, res) {
+  res.render('my_form', {
     title: 'my main page'
   })
 })
@@ -34,9 +51,9 @@ app.get('/', function(req, res){
 
 
 // Server startup
-const port  = server_config.server_port
+const port = server_config.server_port
 app.listen(port, function () {
-    logger.info(server_config.app_name+' listening on port ', port)
+  logger.info(server_config.app_name + ' listening on port ', port)
 })
 
 
